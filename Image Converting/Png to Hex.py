e73f45a97b8b23091575c37ae0382671260fdecd
Robert <hud362217115@gmail.com>
2024-02-12 15:51:13 -0500

    played with png to hex

--- Image Converting/Png to Hex.py
+++ Image Converting/Png to Hex.py
@@ -1,7 +1,14 @@
 import binascii
-filename = '480x480.png'
+from PIL import Image
+from random import randint
+print(randint(0,3))
+image='image'+str(randint(0,3))+'.png'
+filename = image
+
 with open(filename, 'rb') as f:
     content = f.read()
+    im = Image.open(filename)
+    im.show(filename)
 print(binascii.hexlify(content).decode())
 f = open('text.txt', 'w')
 f.write(binascii.hexlify(content).decode())
