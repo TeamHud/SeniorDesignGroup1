e15405f797ccfc6a0bdef827330d2bdb3da000f2
TeamHud <hud362217115@gmail.com>
2024-03-18 09:11:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ import termios
 import tty
 from threading import Timer
 import subprocess
+#import used libraries 
+
 subprocess.Popen(["python",'Take.py'])
+#load and run the script to take a picture
+
 old_settings = termios.tcgetattr(sys.stdin)
 tty.setcbreak(sys.stdin.fileno())
 node = sx126x.sx126x(serial_num = "/dev/ttyS0",freq=868,addr=0,power=22,rssi=True,air_speed=2400,relay=False)
+#define transmission settings
+
 def send_timer(continue_or_not = True):
     with open('/home/cubesat/sciprts/text.txt') as file:
         f=file.read()  
@@ -29,7 +35,7 @@ def send_timer(continue_or_not = True):
             timer_task = Timer(seconds,send_timer)
             timer_task.start()
             print(out[i])
-
+#define the function that opens the file to be transmitted and transmits it.
 
 try:
     time.sleep(1)
@@ -43,16 +49,7 @@ try:
             break
             sys.stdout.flush()
         node.receive()
-        
+#set up timer task to transmit every 60 seconds and set up break        
 except:
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
-    # print('\x1b[2A',end='\r')
-    # print(" "*100)
-    # print(" "*100)
-    # print('\x1b[2A',end='\r')
-
 termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
-# print('\x1b[2A',end='\r')
-# print(" "*100)
-# print(" "*100)
-# print('\x1b[2A',end='\r')
