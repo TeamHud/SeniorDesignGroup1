b829ed6a241a35c7815c7400295662c902c4169d
TeamHud <hud362217115@gmail.com>
2024-03-19 19:26:26 -0400

    Update Take.py

--- CubeSat/Take.py
+++ CubeSat/Take.py
@@ -9,11 +9,15 @@ from threading import Timer
 import time
 from picamera2 import Picamera2, Preview
 import binascii
+#import used libraries
+
 picam = Picamera2()
 config = picam.create_preview_configuration(main={"size":(480,480)})
 picam.configure(config)
 picam.options['quality']=5
 picam.start()
+#initilize PiCamera and configure picture settings
+
 def take_pic (continue_or_not = True):
 	if continue_or_not:
 		global timer_task
@@ -30,7 +34,11 @@ def take_pic (continue_or_not = True):
 		f=open('text.txt','w')
 		f.write(binascii.hexlify(content).decode())
 		f.close()
-	
+#set up timer task for taking a picture every minute
+#take picture, create a text file, then convert picture to text
+#finally save picture as text to text file
+
+
 try:
     time.sleep(.1)
     print("Press \033[1;32mEsc\033[0m to exit")
@@ -40,6 +48,7 @@ try:
         timer_task.start()
         if sys.stdin.read(1) == '\x1b':
             timer_task.cancel()
+#code to 		
 except:
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
