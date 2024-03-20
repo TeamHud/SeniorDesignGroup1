b868dfdd97095fe83740fc845a1a2142154d645e
TeamHud <hud362217115@gmail.com>
2024-03-20 11:15:10 -0400

    Update recieve.py

--- Ground Station/recieve.py
+++ Ground Station/recieve.py
@@ -11,10 +11,7 @@ import shutil
 old_settings = termios.tcgetattr(sys.stdin)
 tty.setcbreak(sys.stdin.fileno())
 node = sx126x.sx126x(serial_num = "/dev/ttyS0",freq=868,addr=0,power=22,rssi=True,air_speed=2400,relay=False)
-
-
-print("press esc to stop recieving")
-
+#import used libraries and initilize the sx126x 
 
 folder = '/home/groundstation/Downloads/SeniorDesignGroup1-main/recieved data'
 for filename in os.listdir(folder):
@@ -26,29 +23,39 @@ for filename in os.listdir(folder):
             shutil.rmtree(file_path)
     except Exception as e:
         print('Failed to delete %s. Reason: %s' % (file_path, e))
+#path to the folder for storing recieved data and clear it on initilization 
+
 time.sleep(1)
 t=0
 f=open('recieved data/r0.txt','x')
+#create and open starting file
+
 while True:
     time.sleep(.5)
     n=node.receive()
-    if n != None:
+#check for data from satellite
+
+        if n != None:    
         words = set(n.split())
         n=n[:-1]
         n=n[3:]
         print (words)
+#If reciever recieves data trim the data to remove addressing informtion
+            
         if 'end' in words:
             t=t+1
             newfile='recieved data/''r'+str(t)+'.txt'
             f=open(newfile,'x')
             print(t)
+#if the data package contains the 'end' string then open a new file  
+            
         else:
             newfile='recieved data/''r'+str(t)+'.txt'
             f=open(newfile,'a')
             f.write(n)
             f.close()
             print(n)
-
+#if the data does not contain an end sting continue writing the data to the oritinal file
 
 
 # while True:
